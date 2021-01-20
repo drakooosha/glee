4eb1453e62379e763b61472702c621628a1cfd3f
drakooosha <kopteloy@gmail.com>
2021-01-20 04:12:10 +0300

    made 12 task

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-    console.log('hi')
+$(function(){  
+    $('.advantages__slider').slick({
+        arrows: false,
+        dots: true
+    });
+
+    var mixer = mixitup('.products__items');
 })
