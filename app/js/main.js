4e809d38d303d3648b2e956377c7a5d47953b08c
drakooosha <kopteloy@gmail.com>
2021-02-03 15:57:59 +0300

    fixed 13task + responsive

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,33 @@
 $(function () {
-    $('.advantages__slider').slick({
+    $('.menu__phone-btn').on('click', function () {
+        $('.menu__user').toggleClass('active');
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list,.menu__btn').toggleClass('active');
+    });
+
+
+    $('.advantages-slider').slick({
         arrows: false,
         dots: true
     });
 
+    $('.partners-slider').slick({
+        arrows: false,
+        autoplay: true
+    });
+
     var mixer1 = mixitup('.products__items', {
         selectors: {
-            control: '.products__filter-btn'
+            control: '.js-products__filter-btn'
         }
     });
 
     var mixer2 = mixitup('.design__items', {
         selectors: {
-            control: '.design__filter-btn'
+            control: '.js-design__filter-btn'
         }
     });
 
 })
-
